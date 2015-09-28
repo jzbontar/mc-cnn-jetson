8ebbc19e8553c73b023f4a1c89eaa114cd386e94
Jure Zbontar <jure.zbontar@gmail.com>
2015-09-28 14:40:38 -0400

    spaces -> tabs

--- stereo.cu
+++ stereo.cu
@@ -14,101 +14,101 @@ cudnnHandle_t cudnn_handle;
 
 /* Tensor */
 struct Tensor {
-    float *data;
-    cudnnTensorDescriptor_t desc;
-    int n, c, h, w, size, capacity;
+	float *data;
+	cudnnTensorDescriptor_t desc;
+	int n, c, h, w, size, capacity;
 };
 
 void Tensor_init(struct Tensor *t)
 {
-    t->data = NULL;
-    t->n = t->c = t->h = t->w = t->size = t->capacity = 0;
-    ok(cudnnCreateTensorDescriptor(&t->desc));
+	t->data = NULL;
+	t->n = t->c = t->h = t->w = t->size = t->capacity = 0;
+	ok(cudnnCreateTensorDescriptor(&t->desc));
 }
 
 void Tensor_resize(struct Tensor *t, int n, int c, int h, int w)
 {
-    int size = n * c * h * w;
+	int size = n * c * h * w;
 
-    if (t->capacity != 0 && size > t->capacity) {
-        printf("DNN: reallocating tensor\n");
-        ok(cudaFree(t->data));
-        t->capacity = 0;
-    }
+	if (t->capacity != 0 && size > t->capacity) {
+		printf("DNN: reallocating tensor\n");
+		ok(cudaFree(t->data));
+		t->capacity = 0;
+	}
 
-    if (t->capacity == 0) {
-        t->capacity = size;
-        ok(cudaMalloc(&t->data, t->capacity * 4));
-    }
-    t->n = n;
-    t->c = c;
-    t->h = h;
-    t->w = w;
-    t->size = size;
+	if (t->capacity == 0) {
+		t->capacity = size;
+		ok(cudaMalloc(&t->data, t->capacity * 4));
+	}
+	t->n = n;
+	t->c = c;
+	t->h = h;
+	t->w = w;
+	t->size = size;
 
-    ok(cudnnSetTensor4dDescriptor(t->desc, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, n, c, h, w))
+	ok(cudnnSetTensor4dDescriptor(t->desc, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, n, c, h, w))
 }
 
 void Tensor_init_resize(struct Tensor *t, int n, int c, int h, int w)
 {
-    Tensor_init(t);
-    Tensor_resize(t, n, c, h, w);
+	Tensor_init(t);
+	Tensor_resize(t, n, c, h, w);
 }
 
 
 /* ConvLayer */
 struct ConvLayer {
 	int relu;
-    struct Tensor output, weight, bias;
-    cudnnConvolutionFwdAlgo_t algorithm;
-    cudnnFilterDescriptor_t weight_desc;
-    cudnnConvolutionDescriptor_t conv_desc;
+	struct Tensor output, weight, bias;
+	cudnnConvolutionFwdAlgo_t algorithm;
+	cudnnFilterDescriptor_t weight_desc;
+	cudnnConvolutionDescriptor_t conv_desc;
 };
 
 void ConvLayer_init(ConvLayer *e, int n_in, int n_out, int kw, int kh, int sx, int sy, int padw, int padh, int relu)
 {
-    Tensor_init(&e->output);
-    Tensor_init_resize(&e->weight, n_out, n_in, kh, kw);
-    Tensor_init_resize(&e->bias, 1, n_out, 1, 1);
-    ok(cudnnCreateFilterDescriptor(&e->weight_desc));
-    ok(cudnnSetFilter4dDescriptor(e->weight_desc, CUDNN_DATA_FLOAT, n_out, n_in, kh, kw));
-    ok(cudnnCreateConvolutionDescriptor(&e->conv_desc));
-    ok(cudnnSetConvolution2dDescriptor(e->conv_desc, padh, padw, sy, sx, 1, 1, CUDNN_CONVOLUTION));
+	Tensor_init(&e->output);
+	Tensor_init_resize(&e->weight, n_out, n_in, kh, kw);
+	Tensor_init_resize(&e->bias, 1, n_out, 1, 1);
+	ok(cudnnCreateFilterDescriptor(&e->weight_desc));
+	ok(cudnnSetFilter4dDescriptor(e->weight_desc, CUDNN_DATA_FLOAT, n_out, n_in, kh, kw));
+	ok(cudnnCreateConvolutionDescriptor(&e->conv_desc));
+	ok(cudnnSetConvolution2dDescriptor(e->conv_desc, padh, padw, sy, sx, 1, 1, CUDNN_CONVOLUTION));
 	e->relu = relu;
 }
 
 struct Tensor *ConvLayer_allocate(ConvLayer *e, struct Tensor *i) {
-    int n, c, h, w;
-    ok(cudnnGetConvolution2dForwardOutputDim(e->conv_desc, i->desc, e->weight_desc, &n, &c, &h, &w));
-    Tensor_resize(&e->output, n, c, h, w);
-    ok(cudnnGetConvolutionForwardAlgorithm(cudnn_handle, i->desc, e->weight_desc, e->conv_desc,
-        e->output.desc, CUDNN_CONVOLUTION_FWD_PREFER_FASTEST, 0, &e->algorithm));
-    return &e->output;
+	int n, c, h, w;
+	ok(cudnnGetConvolution2dForwardOutputDim(e->conv_desc, i->desc, e->weight_desc, &n, &c, &h, &w));
+	Tensor_resize(&e->output, n, c, h, w);
+	ok(cudnnGetConvolutionForwardAlgorithm(cudnn_handle, i->desc, e->weight_desc, e->conv_desc,
+		e->output.desc, CUDNN_CONVOLUTION_FWD_PREFER_FASTEST, 0, &e->algorithm));
+	return &e->output;
 }
 
 struct Tensor *ConvLayer_forward(ConvLayer *e, struct Tensor *i) {
-    int zero = 0;
-    int one = 1;
-    ok(cudnnConvolutionForward(cudnn_handle, &one, i->desc, i->data, e->weight_desc, e->weight.data,
-        e->conv_desc, e->algorithm, NULL, 0, &zero, e->output.desc, e->output.data));
-    ok(cudnnAddTensor(cudnn_handle, CUDNN_ADD_SAME_C, &one, e->bias.desc, e->bias.data, &one,
-        e->output.desc, e->output.data));
+	int zero = 0;
+	int one = 1;
+	ok(cudnnConvolutionForward(cudnn_handle, &one, i->desc, i->data, e->weight_desc, e->weight.data,
+		e->conv_desc, e->algorithm, NULL, 0, &zero, e->output.desc, e->output.data));
+	ok(cudnnAddTensor(cudnn_handle, CUDNN_ADD_SAME_C, &one, e->bias.desc, e->bias.data, &one,
+		e->output.desc, e->output.data));
 	if (e->relu) {
 		ok(cudnnActivationForward(cudnn_handle, CUDNN_ACTIVATION_RELU, &one, e->output.desc, 
 			e->output.data, &zero, e->output.desc, e->output.data));
 	}
-    return &e->output;
+	return &e->output;
 }
 
 /* Sequential */
 struct Sequential {
-    struct ConvLayer modules[32];
-    int num_modules;
+	struct ConvLayer modules[32];
+	int num_modules;
 };
 
 void Sequential_init(struct Sequential *s)
 {
-    s->num_modules = 0;
+	s->num_modules = 0;
 }
 
 void Sequential_load(struct Sequential *s, const char *dir)
@@ -130,34 +130,34 @@ void Sequential_load(struct Sequential *s, const char *dir)
 
 struct Tensor *Sequential_allocate(struct Sequential *s, struct Tensor *input)
 {
-    struct Tensor *output = input;
-    for (int i = 0; i < s->num_modules; i++) {
-        output = ConvLayer_allocate(s->modules + i, output);
-    }
-    return output;
+	struct Tensor *output = input;
+	for (int i = 0; i < s->num_modules; i++) {
+		output = ConvLayer_allocate(s->modules + i, output);
+	}
+	return output;
 }
 
 struct Tensor *Sequential_forward(struct Sequential *s, struct Tensor *input)
 {
-    struct Tensor *output = input;
-    for (int i = 0; i < s->num_modules; i++) {
-        output = ConvLayer_forward(s->modules + i, output);
-    }
-    return output;
+	struct Tensor *output = input;
+	for (int i = 0; i < s->num_modules; i++) {
+		output = ConvLayer_forward(s->modules + i, output);
+	}
+	return output;
 }
 
 double get_time()
 {
-    struct timeval t;
-    struct timezone tzp;
-    gettimeofday(&t, &tzp);
-    return t.tv_sec + t.tv_usec * 1e-6;
+	struct timeval t;
+	struct timezone tzp;
+	gettimeofday(&t, &tzp);
+	return t.tv_sec + t.tv_usec * 1e-6;
 }
 
 __global__ void zero_(float *input, int size)
 {
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
-    if (id < size) {
+	int id = blockIdx.x * blockDim.x + threadIdx.x;
+	if (id < size) {
 		input[id] = 0;
 	}
 }
@@ -169,8 +169,8 @@ void zero(Tensor *t)
 
 __global__ void mul_(float *input, float factor, int size)
 {
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
-    if (id < size) {
+	int id = blockIdx.x * blockDim.x + threadIdx.x;
+	if (id < size) {
 		input[id] = input[id] * factor;
 	}
 }
@@ -215,35 +215,35 @@ void gray2display(Tensor *input, unsigned char *display)
 
 __global__ void ad_(float *x0, float *x1, float *output, int size, int size2, int size3, int direction)
 {
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
+	int id = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (id < size) {
-        int d = id;
-        int x = d % size3;
-        d /= size3;
-        int y = d % size2;
-        d /= size2;
-        d *= direction;
+	if (id < size) {
+		int d = id;
+		int x = d % size3;
+		d /= size3;
+		int y = d % size2;
+		d /= size2;
+		d *= direction;
 
-        float dist;
-        if (0 <= x + d && x + d < size3) {
-            int cnt = 0;
-            dist = 0;
-            for (int yy = y - 2; yy <= y + 2; yy++) {
-                for (int xx = x - 2; xx <= x + 2; xx++) {
-                    if (0 <= xx && xx < size3 && 0 <= xx + d && xx + d < size3 && 0 <= yy && yy < size2) {
-                        int ind = yy * size3 + xx;
-                        dist += abs(x0[ind] - x1[ind + d]);
-                        cnt++;
-                    }
-                }
-            }
-            dist /= cnt;
-        } else {
-            dist = CUDART_NAN;
-        }
-        output[id] = dist;
-    }
+		float dist;
+		if (0 <= x + d && x + d < size3) {
+			int cnt = 0;
+			dist = 0;
+			for (int yy = y - 2; yy <= y + 2; yy++) {
+				for (int xx = x - 2; xx <= x + 2; xx++) {
+					if (0 <= xx && xx < size3 && 0 <= xx + d && xx + d < size3 && 0 <= yy && yy < size2) {
+						int ind = yy * size3 + xx;
+						dist += abs(x0[ind] - x1[ind + d]);
+						cnt++;
+					}
+				}
+			}
+			dist /= cnt;
+		} else {
+			dist = CUDART_NAN;
+		}
+		output[id] = dist;
+	}
 }
 
 void ad(Tensor *x0, Tensor *x1, Tensor *mc, int disp_max, int direction)
@@ -255,19 +255,19 @@ void ad(Tensor *x0, Tensor *x1, Tensor *mc, int disp_max, int direction)
 
 __global__ void argmin_(float *input, float *output, int size1, int size23)
 {
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
-    if (id < size23) {
-        int argmin = 0;
-        float min = CUDART_INF;
-        for (int i = 0; i < size1; i++) {
-            float val = input[i * size23 + id];
-            if (val < min) {
-                min = val;
-                argmin = i;
-            }
-        }
-        output[id] = argmin;
-    }
+	int id = blockIdx.x * blockDim.x + threadIdx.x;
+	if (id < size23) {
+		int argmin = 0;
+		float min = CUDART_INF;
+		for (int i = 0; i < size1; i++) {
+			float val = input[i * size23 + id];
+			if (val < min) {
+				min = val;
+				argmin = i;
+			}
+		}
+		output[id] = argmin;
+	}
 }
 
 void argmin(Tensor *input, Tensor *output)
@@ -279,8 +279,8 @@ void argmin(Tensor *input, Tensor *output)
 
 __global__ void downsample_(float *input, float *output, int factor, int size3, int size)
 {
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
-    if (id < size) {
+	int id = blockIdx.x * blockDim.x + threadIdx.x;
+	if (id < size) {
 		int dim3 = id % size3;
 		int dim2 = id / size3;
 		atomicAdd(output + ((dim2 / factor) * (size3 / factor) + (dim3 / factor)), input[id] / (factor * factor));
@@ -308,7 +308,7 @@ Sequential net;
 Tensor x0_gray_big, x1_gray_big, x0_gray, x1_gray, x0_mc, x0_disp, batch;
 int width_big, height_big, size_big, width, height, size;
 
-int downsample_factor = 4;
+int downsample_factor = 5;
 int disp_max = 64;
 
 void stereo_init(int width_arg, int height_arg)
@@ -321,16 +321,16 @@ void stereo_init(int width_arg, int height_arg)
 	Tensor_init(&x0_disp);
 	Tensor_init(&batch);
 
-    width_big = width_arg;
-    height_big = height_arg;
-    size_big = width_big * height_big;
+	width_big = width_arg;
+	height_big = height_arg;
+	size_big = width_big * height_big;
 
-    assert(width_big % downsample_factor == 0);
-    assert(height_big % downsample_factor == 0);
+	assert(width_big % downsample_factor == 0);
+	assert(height_big % downsample_factor == 0);
 
-    width = width_big / downsample_factor;
-    height = height_big / downsample_factor;
-    size = width * height;
+	width = width_big / downsample_factor;
+	height = height_big / downsample_factor;
+	size = width * height;
 
 	ok(cudnnCreate(&cudnn_handle));
 
@@ -339,7 +339,7 @@ void stereo_init(int width_arg, int height_arg)
 	Sequential_load(&net, "tmp/foo");
 	Sequential_allocate(&net, &batch);
 
-    printf("stereo_init: %d x %d\n", width, height);
+	printf("stereo_init: %d x %d\n", width, height);
 }
 
 void stereo_run(unsigned char *x0, unsigned char *x1, unsigned char *display)
